a57f1b6a0f946d39dc81681257e7f0b8605daa86
Gouzhong <1162864960@qq.com>
2020-03-28 12:31:31 +0800

    新增配置类

---
+++ src/main/java/com/gouzhong1223/springbootresourcesanalysis/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.gouzhong1223.springbootresourcesanalysis.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : 自定义配置类
+ * @Date : create by QingSong in 2020-03-28 12:28 下午
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : com.gouzhong1223.springbootresourcesanalysis.config
+ * @ProjectName : springboot-resources-analysis
+ * @Version : 1.0.0
+ */
+@Configuration
+@ComponentScan("com.gouzhong1223.springbootresourcesanalysis")
+public class ApplicationConfig {
+
+}
